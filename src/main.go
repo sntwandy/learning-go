dfbd81ef8adc9065561667f74c17b76452010aea
sntwandy <sntwandy@gmail.com>
2022-07-16 11:01:11 -0400

    feat: ✨Init variables, constants and Zero values

--- src/main.go
+++ src/main.go
@@ -3,5 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Init constants
+	const hello = "Hello, World!"
+	const pi float64 = 3.141592653589793
+	const pi2 = 3.141592653589793
+
+	// Init integer variables
+	base := 17
+	var height int = 14
+	var area int
+
+	fmt.Println("Base: ", base)
+	fmt.Println("Height: ", height)
+	fmt.Println("Area: ", area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println("a: ", a)
+	fmt.Println("b: ", b)
+	fmt.Println("c: ", c)
+	fmt.Println("d: ", d)
+
+	// Square area
+	const squareBase = 10
+	squareArea := squareBase * squareBase
+
+	fmt.Println("Square area: ", squareArea)
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
 }

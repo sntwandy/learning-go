c6e37ddce8b120036d137eafb6d37cdc38137294
sntwandy <sntwandy@gmail.com>
2022-07-16 11:22:06 -0400

    feat: ✨Arithmetic operators

--- src/main.go
+++ src/main.go
@@ -3,30 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// Init constants
-	const hello = "Hello, World!"
-	const pi float64 = 3.141592653589793
-	const pi2 = 3.141592653589793
-
-	// Init integer variables
-	base := 17
-	var height int = 14
-	var area int
-
-	fmt.Println("Base: ", base)
-	fmt.Println("Height: ", height)
-	fmt.Println("Area: ", area)
-
-	// Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println("a: ", a)
-	fmt.Println("b: ", b)
-	fmt.Println("c: ", c)
-	fmt.Println("d: ", d)
 
 	// Square area
 	const squareBase = 10
@@ -34,6 +10,55 @@ func main() {
 
 	fmt.Println("Square area: ", squareArea)
 
-	fmt.Println("pi: ", pi)
-	fmt.Println("pi2: ", pi2)
+	x:= 10
+	y:= 50
+
+	// Addition
+	result:= x + y
+	fmt.Println("Addition: ", result)
+
+	// Subtraction
+	result = y - x
+	fmt.Println("Subtraction: ", result)
+
+	// Multiplication
+	result = x * y
+	fmt.Println("Multiplication: ", result)
+
+	// Division
+	result = y / x
+	fmt.Println("Division: ", result)
+
+	// Modulus
+	result = y % x
+	fmt.Println("Modulus: ", result)
+
+	// Increment
+	x++
+	fmt.Println("Increment: ", x)
+
+	// Decrement
+	x--
+	fmt.Println("Decrement: ", x)
+
+	// Reactangle area
+	const rectangleBase = 10
+	const rectangleHeight = 5
+	rectangleArea := rectangleBase * rectangleHeight
+
+	fmt.Println("Rectangle area: ", rectangleArea)
+
+	// Trapeze area
+	const trapezeBase = 10
+	const trapezeHeight = 5
+	trapezeArea := (trapezeBase + trapezeHeight) / 2
+
+	fmt.Println("Trapeze area: ", trapezeArea)
+
+	// Circle area
+	const circleRadius = 10
+	const PI = 3.14
+	circleArea := PI * circleRadius * circleRadius
+
+	fmt.Println("Circle area: ", circleArea)
 }
